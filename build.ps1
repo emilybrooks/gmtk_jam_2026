d407f399c6b9f340cfdd1a4e93b2cfc635fd89e5
emilybrooks <emilybrooksemilybrooks@gmail.com>
2026-07-20 08:51:46 -0700

    remove 7zip from build script

--- build.ps1
+++ build.ps1
@@ -17,24 +17,26 @@ $godot_output = Join-Path $windows_x64_release_dir "$($exe_name).exe"
 godot --headless --path $godot_dir --export-release "Windows Desktop" $godot_output
 # Stop the script if the previous line returns a non-zero exit code.
 if(!$?) { Exit $LASTEXITCODE } 
-Copy-Item -Path $license_dir -Destination $(Join-Path $windows_x64_release_dir "license") -Recurse
+$license_output = Join-Path $windows_x64_release_dir "license"
+Copy-Item -Path $license_dir -Destination $license_output -Recurse
 
 # zip windows x64
 $zip_output = Join-Path $release_dir "windows_x64.zip"
 $zip_files = Join-Path $windows_x64_release_dir "*"
-7z a $zip_output $zip_files
+Compress-Archive -Path $zip_files -DestinationPath $zip_output
 
 # build linux
 New-Item -Type dir $linux_release_dir > $null
 $godot_output = Join-Path $linux_release_dir "$($exe_name).x86_64"
 godot --headless --path $godot_dir --export-release "Linux" $godot_output
 if(!$?) { Exit $LASTEXITCODE }
-Copy-Item -Path $license_dir -Destination $(Join-Path $linux_release_dir "license") -Recurse
+$license_output = Join-Path $linux_release_dir "license"
+Copy-Item -Path $license_dir -Destination $license_output -Recurse
 
 # zip linux
 $zip_output = Join-Path $release_dir "linux_x64.zip"
 $zip_files = Join-Path $linux_release_dir "*"
-7z a $zip_output $zip_files
+Compress-Archive -Path $zip_files -DestinationPath $zip_output
 
 # build web
 New-Item -Type dir $web_release_dir > $null
@@ -45,4 +47,4 @@ if(!$?) { Exit $LASTEXITCODE }
 # zip web
 $zip_output = Join-Path $release_dir "web.zip"
 $zip_files = Join-Path $web_release_dir "*"
-7z a $zip_output $zip_files
+Compress-Archive -Path $zip_files -DestinationPath $zip_output
