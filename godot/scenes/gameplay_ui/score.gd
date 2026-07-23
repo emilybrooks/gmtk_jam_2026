22a5f417279bb28af9acb9ae0056379b4e578009
emilybrooks <emilybrooksemilybrooks@gmail.com>
2026-07-23 09:10:57 -0700

    score label now resets

--- godot/scenes/gameplay_ui/score.gd
+++ godot/scenes/gameplay_ui/score.gd
@@ -2,16 +2,15 @@ extends Label
 
 var score: int = 0
 
-# TODO this is bad!!! We have two sources of the same information. Only one of these should exist, and the UI probably shouldn't be the one that holds it
-var collected_goals = []
-
 func _ready() -> void:
 	update_label()
 
 func update_label() -> void:
-	text = "[%s]" % ", ".join(collected_goals)
+	text = "[%s]" % ", ".join(%Gate.collected_goals)
 
 func _on_goal_item_collected(number: int) -> void:
-	collected_goals.append(number)
 	update_label()
-	
+
+func _on_game_init() -> void:
+	text = "[]"
+		
