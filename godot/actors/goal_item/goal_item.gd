044b1155fe20298adc0d02b828030c9b597fb570
Tom Overton <tom.overton@outlook.com>
2026-07-22 21:41:17 -0700

    Add numbers to the goal items

--- godot/actors/goal_item/goal_item.gd
+++ godot/actors/goal_item/goal_item.gd
@@ -4,6 +4,9 @@ signal goal_item_collected(number)
 
 @export var number = 1
 
+func _ready() -> void:
+	$Label3D.text = "%d" % number
+
 func _on_area_3d_area_entered(area: Area3D) -> void:
 	goal_item_collected.emit(number)
 	queue_free()
