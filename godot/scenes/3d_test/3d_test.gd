2a252820eff3b51f042c56b58dc8ff607517548d
Tom Overton <tom.overton@outlook.com>
2026-07-22 18:23:57 -0700

    State handling for the goal + YOU WON!

--- godot/scenes/3d_test/3d_test.gd
+++ godot/scenes/3d_test/3d_test.gd
@@ -5,3 +5,12 @@ func _ready() -> void:
 	for score_item in score_items:
 		score_item.score_item_collected.connect(%LabelScore._on_score_item_collected)
 	
+
+
+func _on_goal_goal_ready() -> void:
+	$%LabelGoalDebug.text = "Goal Status: Ready"
+
+
+func _on_goal_goal_complete() -> void:
+	$%LabelGoalDebug.text = "Goal Status: Complete"
+	$%LabelYouWon.visible = true
