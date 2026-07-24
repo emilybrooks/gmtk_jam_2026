f8bb240159e545f081ee9a3dca251ecfecf873e0
Tom Overton <tom.overton@outlook.com>
2026-07-23 17:23:40 -0700

    Add polish and SFX to spring

--- godot/actors/spring/spring.gd
+++ godot/actors/spring/spring.gd
@@ -2,21 +2,40 @@ extends Node3D
 
 signal spring_touched
 
-@onready var can_bounce = true
+@onready var bounce_cooldown = false
+@onready var squash_and_stretch = false
 
 func _ready() -> void:
 	pass
 
 func _process(delta: float) -> void:
-	pass
+	if squash_and_stretch:
+		var ratio = $SquashAndStretchTimer.time_left / $SquashAndStretchTimer.wait_time
+		var angle = 6 * PI * ratio
+		var horiz_scale = 0.4 * cos(angle)
+		var vert_scale = 0.6 * sin(angle)
+
+		$Area3D.scale.x = 1.0 + (horiz_scale * ratio)
+		$Area3D.scale.z = 1.0 + (horiz_scale * ratio)
+		$Area3D.scale.y = 1.0 + (vert_scale * ratio)
+	else:
+		$Area3D.scale.x = 1.0
+		$Area3D.scale.z = 1.0
+		$Area3D.scale.y = 1.0
 
 func _on_area_3d_area_entered(area: Area3D) -> void:
-	if (can_bounce):
+	if (!bounce_cooldown):
 		spring_touched.emit()
 		$CooldownTimer.start()
-		can_bounce = false
-
+		$SquashAndStretchTimer.start()
+		$BoingSound.play()
+		bounce_cooldown = true
+		squash_and_stretch = true
 
 func _on_cooldown_timer_timeout() -> void:
-	print("cooldown done")
-	can_bounce = true
+	print("[Spring] cooldown done")
+	bounce_cooldown = false
+
+func _on_squash_and_stretch_timer_timeout() -> void:
+	print("[Spring] squash and stretch done")
+	squash_and_stretch = false
