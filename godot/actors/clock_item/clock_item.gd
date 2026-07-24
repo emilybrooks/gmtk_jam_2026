04b4dab3349c3ae78cbc864960d70ce93a5d9056
Tom Overton <tom.overton@outlook.com>
2026-07-24 04:29:59 -0700

    Polish the clock item

--- godot/actors/clock_item/clock_item.gd
+++ godot/actors/clock_item/clock_item.gd
@@ -1,9 +1,27 @@
 extends Node3D
 
+const REVOLUTIONS_PER_SECOND = 0.5
+const BOBS_PER_SECOND = 1.0
+const BOB_SPEED = 0.4
+
 signal clock_item_collected
+
+@onready var elapsed_time = 0.0
+
+func _process(delta: float) -> void:
+	elapsed_time += delta
+	if (elapsed_time >= 10.0):
+		elapsed_time = 0.0
 	
+	# The clock model is kind of weird and isn't centered, so if you rotate it,
+	# it doesn't look right. Just rotate the entire scene.
+	self.rotation.y += deg_to_rad(360 * REVOLUTIONS_PER_SECOND * delta)
+	
+	$ClockModel.position.y += BOB_SPEED * delta * sin(deg_to_rad(360 * BOBS_PER_SECOND * elapsed_time))
+
 func _on_area_3d_area_entered(area: Area3D) -> void:
 	clock_item_collected.emit()
+	$CollectSound.play()
 	disable()
 
 func enable() -> void:
