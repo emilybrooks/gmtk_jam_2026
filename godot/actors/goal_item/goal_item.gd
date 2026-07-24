058d4f4202243fb0d9ceca5adeada2347c51f0bf
Tom Overton <tom.overton@outlook.com>
2026-07-24 03:29:59 -0700

    Sound effect for goal item

--- godot/actors/goal_item/goal_item.gd
+++ godot/actors/goal_item/goal_item.gd
@@ -3,12 +3,17 @@ extends Node3D
 signal goal_item_collected(number)
 
 @export var number = 1
+var stream = null
 
 func _ready() -> void:
 	$Label3D.text = "%d" % number
+	if (number > 0 and number < 10):
+		stream = load("res://assets/audio/count_%d.ogg" % number)
 
 func _on_area_3d_area_entered(area: Area3D) -> void:
 	goal_item_collected.emit(number)
+	$NumberSound.stream = stream
+	$NumberSound.play()
 	disable()
 
 func enable() -> void:
